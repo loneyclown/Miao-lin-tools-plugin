b367ac58524fb019e3ed419ba43b9ab9aa697e38
40462995@qq.com <40462995@qq.com>
2023-06-14 22:09:42 +0800

    天刀模块公告修复

--- apps/wuxia/index.js
+++ apps/wuxia/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-useless-escape */
 import nodeFetch from 'node-fetch'
-import fs from 'node:fs'
 import _ from 'lodash'
+import iconv from 'iconv-lite'
+import moment from 'moment'
 import LinTools from '../../modules/index.js'
 
 export default class Wuxia extends LinTools {
@@ -11,7 +12,7 @@ export default class Wuxia extends LinTools {
       moduleName: '天涯明月刀工具模块',
       rule: [
         {
-          reg: '天刀公告',
+          reg: '天刀公告(体服)?(最新)?',
           fnc: 'announcement'
         }
       ]
@@ -19,17 +20,29 @@ export default class Wuxia extends LinTools {
   }
 
   async announcement (e) {
+    let tfFlag = false
+    let newFlag = false
+
+    if (e.msg.includes('体服')) {
+      tfFlag = true
+    }
+
+    if (e.msg.includes('最新')) {
+      newFlag = true
+    }
+
     const url = 'https://wuxia.qq.com/webplat/info/news_version3/5012/5013/5014/5016/m3485/list_1.shtml'
     const htmlRes = await nodeFetch(url).then((response) => response.arrayBuffer()).then((buffer) => {
-      const utf8Decoder = new TextDecoder('GBK')
-      const html = utf8Decoder.decode(buffer)
+      // const utf8Decoder = new TextDecoder('utf-8')
+      // const html = utf8Decoder.decode(buffer)
+      const html = iconv.decode(Buffer.from(buffer), 'gbk')
       return html
     })
-    // console.log(htmlRes)
     const liReg = /<li class="news-st"(([\s\S])*?)<\/li>/gi
     const iterator = htmlRes.matchAll(liReg)
 
-    const arr = []
+    let news = { text: '', date: '2000-01-01', url: '' }
+    // const arr = []
     const tArr = []
     for (const item of iterator) {
       if (item && item[0]) {
@@ -37,49 +50,35 @@ export default class Wuxia extends LinTools {
         const url = liStr.match(/<a class="cltit" (.*) href=\"(.*)\">(.*)<\/a>/)[2]
         const text = liStr.match(/<a class="cltit" (.*) href=\"(.*)\">(.*)<\/a>/)[3]
         const date = liStr.match(/<span class="cltime">(.*)<\/span>/)[1]
-        arr.push({
-          liStr,
-          text,
-          date,
-          url: `https://wuxia.qq.com${url}`
-        })
+
+        if (moment(date).diff(moment(news.date)) > 0) {
+          if (tfFlag) {
+            if (text.includes('欢乐英雄')) {
+              news = { text, date, url }
+            }
+          } else if (newFlag) {
+            news = { text, date, url }
+          }
+        }
+
+        // arr.push({
+        //   liStr,
+        //   text,
+        //   date,
+        //   url: `https://wuxia.qq.com${url}`
+        // })
         tArr.push(`【${date}】${text}: https://wuxia.qq.com${url}`)
       }
     }
 
+    logger.info(news)
+
+    if (newFlag) {
+      e.reply(`【${news.date}】${news.text}: https://wuxia.qq.com${news.url}`)
+      return false
+    }
+
     e.reply(this.makeForwardMsg(tArr))
-
-    // console.log(arr)
-
-    // const start = Date.now()
-
-    // const browser = await puppeteer.launch()
-    // const page = await browser.newPage()
-    // await page.goto('https://wuxia.qq.com/webplat/info/news_version3/5012/5013/5014/5016/m3486/202303/931558.shtml')
-
-    // const body = await page.$('body')
-
-    // // // 计算页面高度
-    // // const boundingBox = await body.boundingBox()
-
-    // const buff = await body.screenshot({
-    //   type: 'jpeg',
-    //   omitBackground: false,
-    //   quality: 90,
-    //   path: ''
-    // })
-    // /** 计算图片大小 */
-    // const kb = (buff.length / 1024).toFixed(2) + 'kb'
-    // logger.mark(`[图片生成] ${kb} ${logger.green(`${Date.now() - start}ms`)}`)
-    // // await page.screenshot({ path: '' })
-    // await browser.close()
-
-    // console.log('buff', buff)
-    // const img = segment.image(buff)
-    // e.reply(img)
-    // let utf8decoder = new TextDecoder("GBK");
-    // const t = await grab(url)
-    // e.reply(JSON.stringify(t))
   }
 
   /**
